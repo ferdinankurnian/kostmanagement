cc59d75fba3f052fa479547c8a5244b5273464c5
Ferdinan Kurnian <ferdinankurnian@gmail.com>
2026-04-03 12:41:10 +0700

    fix: position floating action buttons at bottom-right corner

--- apps/web/src/routes/_main/pemilik/_main/informasi.tsx
+++ apps/web/src/routes/_main/pemilik/_main/informasi.tsx
@@ -35,12 +35,9 @@ function RouteComponent() {
         </TopBarCenter>
       </TopBar>
 
-      <div className="fixed inset-x-0 bottom-14 z-50 mx-auto flex max-w-lg justify-end px-4 pointer-events-none">
+      <div className="fixed bottom-14 right-4 z-50">
         <Link to="/pemilik/informasi/add">
-          <Button
-            size="icon-lg"
-            className="pointer-events-auto h-12 w-20 rounded-xl border border-black/20"
-          >
+          <Button size="icon-lg" className="h-12 w-12 rounded-full shadow-lg">
             <Plus className="size-6" />
           </Button>
         </Link>
