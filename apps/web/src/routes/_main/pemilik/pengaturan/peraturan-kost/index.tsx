e08030a1d3627d84aff53ea04c26fbef7a64aa7d
Ferdinan Kurnian <ferdinankurnian@gmail.com>
2026-04-03 12:10:54 +0700

    fix: remove duplicate save button from pemilik peraturan-kost page

--- apps/web/src/routes/_main/pemilik/pengaturan/peraturan-kost/index.tsx
+++ apps/web/src/routes/_main/pemilik/pengaturan/peraturan-kost/index.tsx
@@ -257,23 +257,19 @@ function RouteComponent() {
             ))}
           </div>
         )}
+      </div>
 
+      <div className="fixed bottom-0 left-0 right-0 px-4 pb-4 pt-8 bg-linear-to-t from-background to-transparent">
         <Button
           onClick={handleSave}
           disabled={saveMutation.isPending}
-          className="w-full gap-1.5"
+          className="w-full max-w-lg rounded-full mx-auto"
         >
           {saveMutation.isPending ? (
             <Loader2 className="animate-spin size-4" />
           ) : (
             <Save className="size-4" />
           )}
-          Simpan
-        </Button>
-      </div>
-
-      <div className="fixed bottom-0 left-0 right-0 px-4 pb-4 pt-8 bg-linear-to-t from-background to-transparent text-center">
-        <Button type="submit" className="w-full max-w-lg rounded-full mx-auto">
           Simpan
         </Button>
       </div>
